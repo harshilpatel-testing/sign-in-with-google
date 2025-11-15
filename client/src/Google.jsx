700b221bfb4accaacfc1e2fc2ee29eb5c5571bb8
harshilpatel-testing <harshilpateltesting@gmail.com>
2025-11-15 19:25:26 +0530

    commit

--- client/src/Google.jsx
+++ client/src/Google.jsx
@@ -23,6 +23,7 @@ function Google() {
                     'Content-Type': 'application/json',
                 },
                 body: JSON.stringify({ token: credentialResponse.credential }),
+                credentials: true
             });
 
             const data = await response.json();

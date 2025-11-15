4c859fe65313b816e01c5bfa7627267d41a148f2
harshilpatel-testing <harshilpateltesting@gmail.com>
2025-11-15 20:32:16 +0530

    commit

--- server/server.js
+++ server/server.js
@@ -12,10 +12,7 @@ const app = express();
 
 // Middleware for parsing JSON bodies
 app.use(json());
-app.use(cors({
-    origin: process.env.FRONTEND_URL,
-    methods: ["GET", "POST"],
-}));
+app.use(cors());
 
 let authRoutes = router;
 
